cee91ee652ae292d97c339edaa5372d1a9a8b36b
zainudin noori <zainudinnory@gmail.com>
2017-12-11 13:22:23 +0430

    solved an exception error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+ use Illuminate\Support\Facades\Schema;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -11,11 +11,12 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
-    {
-        //
-    }
 
+
+        public function boot()
+        {
+            Schema::defaultStringLength(191);
+        }
     /**
      * Register any application services.
      *
